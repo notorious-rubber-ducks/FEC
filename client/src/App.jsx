b5212f7ae04acf84ae0eda6b98c351e89f0d459a
N8RB8 <nschroeder95@gmail.com>
2021-10-29 15:40:43 -0600

    Finished server routes and implemented temporary styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Overview from './components/overview/Overview';
-import Questions from './components/questions/Questions';
-import Ratings from './components/ratings/Ratings';
-import Related from './components/related/Related';
+import Overview from './components/overview/Overview.jsx';
+import Questions from './components/questions/Questions.jsx';
+import Ratings from './components/ratings/Ratings.jsx';
+import Related from './components/related/Related.jsx';
 
 export default function App() {
   return (
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const { default: axios } = require('axios');
-const { helper } = require('./server/routes');
+const { APIKEY } = require('./config/config');
 
 const app = express();
 
@@ -9,19 +9,59 @@ app.use(express.static(path.join(__dirname, '/client/dis')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+axios.defaults.headers = {
+  'Content-Type': 'application/json',
+  Authorization: APIKEY,
+  host: 'app-hrsei-api.herokuapp.com',
+};
+
 app.get('/*', (req, res) => {
   // helper function just adds API key to headers
-  //
-  const modifiedheader = helper(req.headers);
+  console.log('GET req initiated');
+  const APIUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-den${req.url}`;
+  console.log(APIUrl);
+  axios
+    .get(APIUrl)
+    .then(({ data }) => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.end();
+    });
+});
+
+app.post('/*', (req, res) => {
+  console.log('POST initiated');
+
   const APIUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-den${req.url}`;
 
-  axios.get(APIUrl, {
-    headers: modifiedheader,
-  })
-    .then((data) => {
-      res.send(data.data);
+  axios
+    .post(APIUrl, req.body)
+    .then(() => {
+      console.log('post successful');
+      res.end();
     })
-    .catch((err) => err);
+    .catch((err) => {
+      console.log(req);
+      console.log(err);
+      res.end();
+    });
+});
+
+app.put('/*', (req, res) => {
+  const APIUrl = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-den${req.url}`;
+
+  axios
+    .put(APIUrl, req.body)
+    .then(() => {
+      res.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      res.end();
+    });
 });
 
 app.listen(3000);
