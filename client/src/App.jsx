2a946adeae530f91622e8d4d23652d12fb1aeb89
Andrew Bunys <andrewbunys@gmail.com>
2021-10-30 11:42:34 -0600

    Setup context for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
 import Overview from './components/overview/Overview.jsx';
 import Questions from './components/questions/Questions.jsx';
 import Ratings from './components/ratings/Ratings.jsx';
 import Related from './components/related/Related.jsx';
+import AppContext from './hooks/context';
 
 export default function App() {
+  const [defaultItem, setDefaultItem] = useState({});
+  let id = '';
+  useEffect(() => {
+    if (defaultItem.id) {
+      id = defaultItem.id;
+    }
+    axios
+      .get(`/products/${id}`)
+      .then(({ data }) => {
+        let useData = data;
+        if (Array.isArray(data)) {
+          [useData] = data;
+        }
+        setDefaultItem(useData);
+      })
+      .catch((err) => err);
+  }, []);
   return (
-    <div>
-      <Overview />
-      <Related />
-      <Questions />
-      <Ratings />
-    </div>
-
+    <AppContext.Provider value={{ defaultItem, setDefaultItem }}>
+      <div>
+        <Overview />
+        <Related />
+        <Questions />
+        <Ratings />
+      </div>
+    </AppContext.Provider>
   );
 }
--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../../hooks/context';
 
 export default function Overview() {
-  //
-};
+  return (
+    <div>
+      <AppContext.Consumer>
+        {(value) => (<div>{value.defaultItem.id}</div>)}
+      </AppContext.Consumer>
+    </div>
+  );
+}
