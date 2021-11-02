e6bd800699626290ea272aa1b5d1397dd4bb062b
Joe Pope <Jokwonpope1561@gmail.com>
2021-11-02 12:27:18 -0600

    OverView

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -2,50 +2,17 @@
 import axios from 'axios';
 import React, { useContext, useState, useEffect } from 'react';
 import {
-  MdOutlineArrowBackIosNew,
-  MdOutlineArrowForwardIos,
-} from 'react-icons/md';
-import {
   AiFillFacebook,
   AiFillInstagram,
   AiFillTwitterCircle,
 } from 'react-icons/ai';
 
 import AppContext from '../../hooks/context';
+import SliderIMG from './SliderIMG.jsx';
+import Selector from './Selectors.jsx';
 
 const iconStyles = { fontSize: '1.5em', margin: '15px', float: 'right' };
-const rightArrow = {
-  position: 'absolute',
-  top: '50%',
-  right: '32px',
-  color: '#000',
-  fontSize: '3rem',
-};
 
-const leftArrow = {
-  position: 'absolute',
-  top: '50%',
-  left: '32px',
-  color: '#000',
-  fontSize: '3rem',
-};
-const slider = {
-  position: 'relative',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'left',
-  alignItems: 'left',
-};
-// const slide = {
-//   opacity: '0',
-//   transitionDuration: '1s ease,',
-// };
-
-// const slideActive = {
-//   opacity: '1',
-//   transitionDuration: '1s',
-//   transform: 'scale(1.08)',
-// };
 export default function Overview() {
   const currentId = String(useContext(AppContext).defaultItem.id);
   const [productView, setProductView] = useState([]);
@@ -58,33 +25,11 @@ export default function Overview() {
       );
   }, []);
 
-  // slider functions
-  const [current, setCurrent] = useState(0);
-  const { length } = productView; // put in array of clothing photos.length
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
   return (
     <div>
       <h3>test</h3>
-      <section style={slider}>
-        <MdOutlineArrowBackIosNew onClick={prevSlide} style={leftArrow} />
-        <MdOutlineArrowForwardIos onClick={nextSlide} style={rightArrow} />
-        {/* insert info from api */}
-        {productView.map((slide, index) => (
-          <div
-            className={index === current ? 'slide active' : 'slide'}
-            key={slide.url}
-          >
-            {index === current && <img src={slide.url} alt="Clothing Img" />}
-          </div>
-        ))}
-      </section>
+      <SliderIMG />
+      <Selector />
       <span>
         <AiFillFacebook id="faceBook" style={iconStyles} />
         <AiFillInstagram id="instagram" style={iconStyles} />
