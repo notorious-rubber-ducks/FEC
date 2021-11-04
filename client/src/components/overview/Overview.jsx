9c47ed4f53b4c6c01f9453b49482be4966bc6e08
N8RB8 <nschroeder95@gmail.com>
2021-11-04 09:40:26 -0600

    added q&a modals and metadata component

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,40 +1,16 @@
 /* eslint-disable implicit-arrow-linebreak */
 import axios from 'axios';
 import React, { useContext, useState, useEffect } from 'react';
-import {
-  AiFillFacebook,
-  AiFillInstagram,
-  AiFillTwitterCircle,
-} from 'react-icons/ai';
 
-import AppContext from '../../hooks/context';
-import SliderIMG from './SliderIMG.jsx';
-import Selector from './Selectors.jsx';
 
-const iconStyles = { fontSize: '1.5em', margin: '15px', float: 'right' };
 
 export default function Overview() {
-  const currentId = String(useContext(AppContext).defaultItem.id);
-  const [productView, setProductView] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`/products/${currentId}/styles`)
-      .then(({ data }) =>
-        data.results.map((item) => setProductView(item.photos))
-      );
-  }, []);
 
   return (
     <div>
       <h3>test</h3>
-      <SliderIMG />
-      <Selector />
-      <span>
-        <AiFillFacebook id="faceBook" style={iconStyles} />
-        <AiFillInstagram id="instagram" style={iconStyles} />
-        <AiFillTwitterCircle id="twitter" style={iconStyles} />
-      </span>
+
     </div>
   );
 }
