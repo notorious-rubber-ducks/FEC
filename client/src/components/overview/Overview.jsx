36f2c13d1c523f7b480a44d3056cd2b5338f8b32
Daniel Pinzon <dpinzon5@gmail.com>
2021-11-08 12:18:03 -0700

    made initial left pane changes and other small updates to index.html and MetaData.jsx

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,45 +1,57 @@
+/* eslint-disable import/extensions */
 /* eslint-disable implicit-arrow-linebreak */
-import axios from 'axios';
 import React, { useContext, useState, useEffect } from 'react';
 import AppContext from '../../hooks/context.js';
 import LeftPanel from './LeftPanel.jsx';
 import RightPanel from './RightPanel.jsx';
 
-
-
 export default function Overview() {
-  let { defaultItem } = useContext(AppContext);
+  const { defaultItem } = useContext(AppContext);
 
-
-  let [currentStyle, setCurrentStyle] = useState({});
-
-  useEffect(()=> {
-    /* define a default style based on the default? property of the results object */
+  function getDefaultStyle() {
     let defaultStyle = defaultItem.results.filter((item) => item['default?'] === true)[0];
     // if there is no default style just use the first item in the results array
     if (defaultStyle === undefined) {
       [defaultStyle] = defaultItem.results;
     }
-    setCurrentStyle(defaultStyle);
-  }, [defaultItem]);
+    return defaultStyle;
+  }
 
+  const [currentStyle, setCurrentStyle] = useState(getDefaultStyle());
+
+  useEffect(() => {
+    setCurrentStyle(getDefaultStyle());
+  }, [defaultItem]);
 
   return (
     <div id="overview">
-      <h2 className='overview-title'>Overview</h2>
-      <div className='flexContainer'>
-        <LeftPanel />
-        <RightPanel currItem={defaultItem} currentStyle={currentStyle} setCurrentStyle={setCurrentStyle} />
+      <h2 className="overview-title">Overview</h2>
+      <div className="flexContainer">
+        <LeftPanel currentStyle={currentStyle} />
+        <RightPanel
+          currItem={defaultItem}
+          currentStyle={currentStyle}
+          setCurrentStyle={setCurrentStyle}
+        />
       </div>
-      <div className='flexContainer'>
-        <span style={{width:'60%'}}>
+      <div className="flexContainer">
+        <span style={{ width: '60%' }}>
           <b>{defaultItem.slogan}</b>
           <br />
           <br />
           {defaultItem.description}
         </span>
-        <span style={{borderLeft:'1px solid black', paddingLeft: 10}}>
-          {defaultItem.features.map(feat => (<div key={feat.feature}><span>✓</span> {feat.feature}: {feat.value}</div>))}
+        <span style={{ borderLeft: '1px solid black', paddingLeft: 10 }}>
+          {defaultItem.features.map((feat) => (
+            <div key={feat.feature}>
+              <span>✓</span>
+              {' '}
+              {feat.feature}
+              :
+              {' '}
+              {feat.value}
+            </div>
+          ))}
         </span>
       </div>
     </div>
